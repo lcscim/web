e80a10a3e07b4d7fb0de2f9750e8bd12da94f40f
lcscim <lvqihui@live.com>
2018-10-27 18:34:00 +0800

    1027 commit.

---
+++ project/1027/perfectCRM/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\asus\Desktop\kaifa\web\project\1027\perfectCRM\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
